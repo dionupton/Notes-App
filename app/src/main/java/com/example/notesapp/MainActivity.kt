cda5a82a4ffdd9261f8a3f7cdff5ff438d1b2a73
dionu <dionupton@protonmail.com>
2020-10-01 00:17:34 +0100

    Layout changes

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.notesapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.realm.Realm
 import io.realm.RealmResults
@@ -37,8 +35,6 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
-
-
     }
 
     private fun setRecyclerView() {
@@ -51,50 +47,5 @@ class MainActivity : AppCompatActivity() {
         notesRV.setHasFixedSize(true)
     }
 
-
-    /*
-    private lateinit var addNotes: FloatingActionButton
-    private lateinit var notesRV:RecyclerView
-    private lateinit var notesList: ArrayList<Note>
-    private lateinit var realm:Realm
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //Init views
-
-        realm = Realm.getDefaultInstance()
-        addNotes = findViewById(R.id.addNotes)
-        notesRV = findViewById(R.id.notesRV)
-
-        //onclick add notes btn
-
-        addNotes.setOnClickListener{
-            startActivity(Intent(this,AddNotesActivity::class.java))
-            finish()
-        }
-
-        notesRV.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
-
-        getAllNotes()
-
-
-    }
-
-
-
-
-
-    private fun getAllNotes() {
-
-        val results:RealmResults<Note> = realm.where<Note>(Note::class.java).findAll()
-
-        notesRV.adapter = NotesAdapter(this, results)
-
-        notesRV.adapter!!.notifyDataSetChanged()
-
-    }
-       */
 
 }
